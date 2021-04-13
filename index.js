eba8d4a5b031b980ca30a54249acbb9b113b0942
rahiemgoedar <79698303+rahiemgoedar@users.noreply.github.com>
2021-04-13 12:19:55 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+let firstName = 'Rahiem';
+console.log(firstName);
+const interestRate = 0.3;
+console.log(interestRate);
+
+//Primitives/Value types//
+let name = 'Rahiem goedar'; //String literal
+let age = 40; //Number Literal
+let isApproved = false; //Boolean Literal
+let selectedColor = null;
+
+let lastName = 'Je let mag niet "if" heten, omdat het een saved command is.';
+console.log(lastName);
+
+let test1 = "4"+"4";
+console.log(test1);
+
+let test2 = 4+4;
+console.log(test2);
+
+let test3 = 4-4;
+console.log(test3);
+
+let test4 = 4*4;
+console.log(test4);
+
+let test5 = 4/4;
+console.log(test5);
+
+let test6 = 4% 100;
+console.log(test6);
+
